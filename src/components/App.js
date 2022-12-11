1d81092d74d744c4ba01b79a95cdf96f67b70b53
VitalijO <onyshko.vitalij@gmail.com>
2022-12-11 22:45:04 +0200

    commit
    commit

--- src/components/App.js
+++ src/components/App.js
@@ -6,21 +6,24 @@ import { Filter } from './Filter/Filter';
 
 
 export class App extends React.Component {
-static defaultProps = {
-   contacts: [],
-  filter: ''
-}
+
 state = {
-  contacts: [
-    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-  ],
+  contacts: [],
   filter: '',
 }
- 
   
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+    }
+  }
+  
+  componentDidMount() {
+    const contactsInStorage = localStorage.getItem('contacts')
+    const parsedContacts = JSON.parse(contactsInStorage)
+    this.setState({contacts: parsedContacts})
+  }
+
   addContact = (name, number) => {
     const existPerson = this.state.contacts.find(person =>
       person.name.toLowerCase() === name.toLowerCase())
